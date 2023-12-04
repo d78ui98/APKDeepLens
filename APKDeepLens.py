829ab4d9c72432d6b29c4c5347d6fd7ae0ae923c
d78ui98 <gajbhiye.deepanshu@gmail.com>
2023-12-04 07:56:07 -0500

    create seperate module for android manifest xml analysis

--- APKDeepLens.py
+++ APKDeepLens.py
@@ -2,12 +2,13 @@ import os
 import subprocess
 import sys
 import re
+import traceback
 import logging
 import argparse
 import time 
-import datetime
 import xml.etree.ElementTree as ET
-from static_tools import sensitive_info_extractor
+import datetime
+from static_tools import sensitive_info_extractor, scan_android_manifest
 from report_gen import ReportGen
 from configparser import ConfigParser
 
@@ -87,7 +88,6 @@ def parse_args():
 
 
     return parser.parse_args()
-    
 
 
 class AutoApkScanner(object):
@@ -125,126 +125,6 @@ class AutoApkScanner(object):
         jadx_path = os.path.join(os.getcwd(), "static_tools", "jadx", "bin", jadx_executable)
         output = subprocess.run([jadx_path, apk_file, "-d", target_dir])
         print(output)
-    
-    def extract_manifest_info(self, apk_file):
-        """
-        Extracts basic information from an Android Manifest file.
-        """
-        extracted_source_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "app_source", apk_file)
-        manifest_path = os.path.join(extracted_source_path, "resources", "AndroidManifest.xml")
-        
-        if not os.path.isfile(manifest_path):
-            util.mod_log(f"[-] ERROR: Manifest file {manifest_path} not found.", util.FAIL)
-
-        etparse = ET.parse(manifest_path)
-        manifest = etparse.getroot()
-
-        if not manifest:
-            util.mod_log(f"[-] ERROR: Error parsing the manifest file for {apk_file}.", util.FAIL)
-
-        android_namespace = '{http://schemas.android.com/apk/res/android}'
-
-        data = {
-        'platformBuildVersionCode': manifest.attrib.get('platformBuildVersionCode', "Not available"),
-        'compileSdkVersion': manifest.attrib.get('compileSdkVersion', "Not available"),
-        'permissions': [elem.attrib[f'{android_namespace}name'] for elem in manifest.findall('uses-permission')],
-        'activities': [elem.attrib[f'{android_namespace}name'] for elem in manifest.findall('application/activity')],
-        'services': [elem.attrib[f'{android_namespace}name'] for elem in manifest.findall('application/service')],
-        'receivers': [elem.attrib[f'{android_namespace}name'] for elem in manifest.findall('application/receiver')],
-        'providers': [elem.attrib[f'{android_namespace}name'] for elem in manifest.findall('application/provider')],
-        'package_name': manifest.attrib.get('package', "Not available")
-    }
-
-        indent = "    "
-
-        DANGEROUS_TYPES = [
-            "android.permission.READ_CALENDAR",
-            "android.permission.WRITE_CALENDAR",
-            "android.permission.CAMERA",
-            "android.permission.READ_CONTACTS",
-            "android.permission.WRITE_CONTACTS",
-            "android.permission.GET_ACCOUNTS",
-            "android.permission.ACCESS_FINE_LOCATION",
-            "android.permission.ACCESS_COARSE_LOCATION",
-            "android.permission.RECORD_AUDIO",
-            "android.permission.READ_PHONE_STATE",
-            "android.permission.READ_PHONE_NUMBERS",
-            "android.permission.CALL_PHONE",
-            "android.permission.ANSWER_PHONE_CALLS",
-            "android.permission.READ_CALL_LOG",
-            "android.permission.WRITE_CALL_LOG",
-            "android.permission.ADD_VOICEMAIL",
-            "android.permission.USE_SIP",
-            "android.permission.PROCESS_OUTGOING_CALLS",
-            "android.permission.BODY_SENSORS",
-            "android.permission.SEND_SMS",
-            "android.permission.RECEIVE_SMS",
-            "android.permission.READ_SMS",
-            "android.permission.RECEIVE_WAP_PUSH",
-            "android.permission.RECEIVE_MMS",
-            "android.permission.READ_EXTERNAL_STORAGE",
-            "android.permission.WRITE_EXTERNAL_STORAGE",
-            "android.permission.MOUNT_UNMOUNT_FILESYSTEMS",
-            "android.permission.READ_HISTORY_BOOKMARKS",
-            "android.permission.WRITE_HISTORY_BOOKMARKS",
-            "android.permission.INSTALL_PACKAGES",
-            "android.permission.RECEIVE_BOOT_COMPLETED",
-            "android.permission.READ_LOGS",
-            "android.permission.CHANGE_WIFI_STATE",
-            "android.permission.DISABLE_KEYGUARD",
-            "android.permission.GET_TASKS",
-            "android.permission.BLUETOOTH",
-            "android.permission.CHANGE_NETWORK_STATE",
-            "android.permission.ACCESS_WIFI_STATE",
-        ]
-        dangerous_permissions = [perm for perm in data['permissions'] if perm in DANGEROUS_TYPES]
-
-        util.mod_log(f"[+] Package Name:", util.OKCYAN)
-        print(indent + data['package_name'] + "\n")
-
-        util.mod_log(f"[+] Platform Build Version Code:", util.OKCYAN)
-        print(indent + str(data['platformBuildVersionCode']) + "\n")
-
-        util.mod_log(f"[+] Compile SDK Version:", util.OKCYAN)
-        print(indent + str(data['compileSdkVersion']) + "\n")
-
-        if data['permissions']:
-            util.mod_log(f"[+] Permissions:", util.OKCYAN)
-            for permission in data['permissions']:
-                print(indent + permission)
-            print()
-        
-        if dangerous_permissions:
-            util.mod_log(f"[+] Dangerous Permissions:", util.FAIL)
-            for permission in dangerous_permissions:
-                print(indent + permission)
-            print()
-
-        if data['activities']:
-            util.mod_log(f"[+] Activities:", util.OKCYAN)
-            for activity in data['activities']:
-                print(indent + activity)
-            print()
-
-        if data['services']:
-            util.mod_log(f"[+] Services:", util.OKCYAN)
-            for service in data['services']:
-                print(indent + service)
-            print()
-
-        if data['receivers']:
-            util.mod_log(f"[+] Receivers:", util.OKCYAN)
-            for receiver in data['receivers']:
-                print(indent + receiver)
-            print()
-
-        if data['providers']:
-            util.mod_log(f"[+] Providers:", util.OKCYAN)
-            for provider in data['providers']:
-                print(indent + provider)
-            print()
-
-        return data
 
     def return_abs_path(self, path):
         '''
@@ -300,7 +180,7 @@ if __name__ == "__main__":
             util.mod_log(f"[-] ERROR: {apk_file_abs_path} not found.", util.FAIL)
             exit(0)
         else:
-            util.mod_print(f"[+] {apk_file_abs_path} found!", util.OKGREEN)
+            util.mod_log(f"[+] {apk_file_abs_path} found!", util.OKGREEN)
         time.sleep(1)
         
         # Extracting source code
@@ -312,7 +192,9 @@ if __name__ == "__main__":
         extracted_apk_path = obj_self.return_abs_path(target_dir["path"])
 
         # Extraction useful infomration from android menifest file
-        obj_self.extract_manifest_info(apk_name)
+        #obj_self.extract_manifest_info(apk_name)
+        extracted_source_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "app_source", apk_name)
+        obj_self = scan_android_manifest.ScanAndroidManifest().extract_manifest_info(extracted_source_path)
     
         # Extracting hardcoded secrets
         obj = sensitive_info_extractor.SensitiveInfoExtractor()
