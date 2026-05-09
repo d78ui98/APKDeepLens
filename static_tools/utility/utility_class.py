836cec4b9241c3a8912b925e44ed3254e1cb49bd
d78ui98 <gajbhiye.deepanshu@gmail.com>
2026-05-09 23:27:58 +0530

    removed redundent definition for dangerous permissions

--- static_tools/utility/utility_class.py
+++ static_tools/utility/utility_class.py
@@ -3,6 +3,47 @@ import traceback
 
 logging.basicConfig(level=logging.DEBUG, format="%(message)s")
 
+DANGEROUS_PERMISSIONS = [
+    "android.permission.READ_CALENDAR",
+    "android.permission.WRITE_CALENDAR",
+    "android.permission.CAMERA",
+    "android.permission.READ_CONTACTS",
+    "android.permission.WRITE_CONTACTS",
+    "android.permission.GET_ACCOUNTS",
+    "android.permission.ACCESS_FINE_LOCATION",
+    "android.permission.ACCESS_COARSE_LOCATION",
+    "android.permission.RECORD_AUDIO",
+    "android.permission.READ_PHONE_STATE",
+    "android.permission.READ_PHONE_NUMBERS",
+    "android.permission.CALL_PHONE",
+    "android.permission.ANSWER_PHONE_CALLS",
+    "android.permission.READ_CALL_LOG",
+    "android.permission.WRITE_CALL_LOG",
+    "android.permission.ADD_VOICEMAIL",
+    "android.permission.USE_SIP",
+    "android.permission.PROCESS_OUTGOING_CALLS",
+    "android.permission.BODY_SENSORS",
+    "android.permission.SEND_SMS",
+    "android.permission.RECEIVE_SMS",
+    "android.permission.READ_SMS",
+    "android.permission.RECEIVE_WAP_PUSH",
+    "android.permission.RECEIVE_MMS",
+    "android.permission.READ_EXTERNAL_STORAGE",
+    "android.permission.WRITE_EXTERNAL_STORAGE",
+    "android.permission.MOUNT_UNMOUNT_FILESYSTEMS",
+    "android.permission.READ_HISTORY_BOOKMARKS",
+    "android.permission.WRITE_HISTORY_BOOKMARKS",
+    "android.permission.INSTALL_PACKAGES",
+    "android.permission.RECEIVE_BOOT_COMPLETED",
+    "android.permission.READ_LOGS",
+    "android.permission.CHANGE_WIFI_STATE",
+    "android.permission.DISABLE_KEYGUARD",
+    "android.permission.GET_TASKS",
+    "android.permission.BLUETOOTH",
+    "android.permission.CHANGE_NETWORK_STATE",
+    "android.permission.ACCESS_WIFI_STATE",
+]
+
 class util:
     """
     A static class for which contain some useful variables and methods
